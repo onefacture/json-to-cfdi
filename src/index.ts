d917df6fb31feb957ef02feb94e1eb05dd4f9fa2
Alan Olivares <richard.alan.oli@gmail.com>
2019-09-29 05:09:01 -0500

    I fix structure

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@ import * as pug from 'pug';
 import * as path from 'path';
 import * as fs from 'fs';
 
-function parse({ jsonXML, pretty }) {
-    let templatePath = path.join(__dirname, 'templates/index.pug');
-    let templateContent = fs.readFileSync(templatePath, 'utf8');
-    let fn = pug.compile(templateContent, {
-        filename: templatePath, pretty
-    });
+const templatePath    = path.join(__dirname, 'templates/index.pug');
+const templateContent = fs.readFileSync(templatePath, 'utf8');
+const parseJsonInline = pug.compile(templateContent, {
+    filename: templatePath
+});
+const parseJsonPretty = pug.compile(templateContent, {
+    filename: templatePath, pretty: true
+});
 
-    return fn(jsonXML);
+const parse = function({ jsonXML, pretty }) {
+	if(pretty) {
+		return parseJsonPretty(jsonXML);
+	}
+
+	return parseJsonInline(jsonXML);
 }
 
-export default { parse };
+export {
+	parse
+};
